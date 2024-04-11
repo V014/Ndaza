d10783bff7b4d6f2025d20b0687cbeb988670b7d
Wanga Kanjala <wangakanjala@gmail.com>
2024-04-11 11:21:19 +0200

    adjusting UI to database

--- Ndaza/Home.Designer.cs
+++ Ndaza/Home.Designer.cs
@@ -33,12 +33,18 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.data_transactions = new MetroFramework.Controls.MetroGrid();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.panel_top = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             ((System.ComponentModel.ISupportInitialize)(this.data_transactions)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // data_transactions
             // 
             this.data_transactions.AllowUserToResizeRows = false;
+            this.data_transactions.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.data_transactions.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(46)))), ((int)(((byte)(56)))));
             this.data_transactions.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.data_transactions.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
@@ -64,7 +70,7 @@
             this.data_transactions.EnableHeadersVisualStyles = false;
             this.data_transactions.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
             this.data_transactions.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.data_transactions.Location = new System.Drawing.Point(67, 0);
+            this.data_transactions.Location = new System.Drawing.Point(0, 0);
             this.data_transactions.Name = "data_transactions";
             this.data_transactions.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
             dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
@@ -77,7 +83,7 @@
             this.data_transactions.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
             this.data_transactions.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
             this.data_transactions.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.data_transactions.Size = new System.Drawing.Size(948, 473);
+            this.data_transactions.Size = new System.Drawing.Size(511, 438);
             this.data_transactions.TabIndex = 0;
             // 
             // panel1
@@ -85,21 +91,46 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(67, 473);
+            this.panel1.Size = new System.Drawing.Size(67, 496);
             this.panel1.TabIndex = 1;
+            // 
+            // panel_top
+            // 
+            this.panel_top.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_top.Location = new System.Drawing.Point(67, 0);
+            this.panel_top.Name = "panel_top";
+            this.panel_top.Size = new System.Drawing.Size(949, 58);
+            this.panel_top.TabIndex = 2;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(67, 58);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.data_transactions);
+            this.splitContainer1.Size = new System.Drawing.Size(949, 438);
+            this.splitContainer1.SplitterDistance = 434;
+            this.splitContainer1.TabIndex = 3;
             // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(27)))), ((int)(((byte)(38)))));
-            this.ClientSize = new System.Drawing.Size(1015, 473);
-            this.Controls.Add(this.data_transactions);
+            this.ClientSize = new System.Drawing.Size(1016, 496);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.panel_top);
             this.Controls.Add(this.panel1);
             this.Name = "Home";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Ndaza ";
             ((System.ComponentModel.ISupportInitialize)(this.data_transactions)).EndInit();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -107,6 +138,8 @@
         #endregion
         private MetroFramework.Controls.MetroGrid data_transactions;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel_top;
+        private System.Windows.Forms.SplitContainer splitContainer1;
     }
 }
 
